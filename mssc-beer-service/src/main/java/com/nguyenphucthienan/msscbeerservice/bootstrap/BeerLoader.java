0d88763f2f922c1600d626a2cf8610bf40688396
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-30 11:39:13 +0700

    Add Beer boostrap

---
+++ mssc-beer-service/src/main/java/com/nguyenphucthienan/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,45 @@
+package com.nguyenphucthienan.msscbeerservice.bootstrap;
+
+import com.nguyenphucthienan.msscbeerservice.domain.Beer;
+import com.nguyenphucthienan.msscbeerservice.repository.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeers();
+    }
+
+    private void loadBeers() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
