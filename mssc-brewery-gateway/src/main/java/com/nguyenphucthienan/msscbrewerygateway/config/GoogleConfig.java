7545b959a43632c0f1ec831e022d2d30a43d0a7c
Nguyen Phuc Thien An <npta97@gmail.com>
2020-06-05 14:29:31 +0700

    Add example route

---
+++ mssc-brewery-gateway/src/main/java/com/nguyenphucthienan/msscbrewerygateway/config/GoogleConfig.java
@@ -0,0 +1,22 @@
+package com.nguyenphucthienan.msscbrewerygateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(route -> route.path("/webhp")
+                        .filters(filter -> filter.rewritePath("/webhp(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com")
+                        .id("google"))
+                .build();
+    }
+}
