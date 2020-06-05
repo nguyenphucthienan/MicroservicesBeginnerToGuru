727980155bc70c48d41b947d924748f240288425
Nguyen Phuc Thien An <npta97@gmail.com>
2020-06-05 09:16:45 +0700

    Add validation failure action

---
+++ mssc-beer-order-service/src/main/java/com/nguyenphucthienan/msscbeerorderservice/sm/action/ValidationFailureAction.java
@@ -0,0 +1,20 @@
+package com.nguyenphucthienan.msscbeerorderservice.sm.action;
+
+import com.nguyenphucthienan.msscbeerorderservice.domain.BeerOrderEventEnum;
+import com.nguyenphucthienan.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import com.nguyenphucthienan.msscbeerorderservice.service.BeerOrderManagerImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ValidationFailureAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        log.error("Compensating Transaction. Validation Failed. BeerOrder ID: " + beerOrderId);
+    }
+}
