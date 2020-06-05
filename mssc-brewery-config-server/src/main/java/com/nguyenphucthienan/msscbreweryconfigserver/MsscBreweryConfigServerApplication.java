2ac2a0cd8900d2286e9ab2446dc98c7d92055b8c
Nguyen Phuc Thien An <npta97@gmail.com>
2020-06-05 23:01:40 +0700

    Init mssc-brewery-config-server project

---
+++ mssc-brewery-config-server/src/main/java/com/nguyenphucthienan/msscbreweryconfigserver/MsscBreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenphucthienan.msscbreweryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryConfigServerApplication.class, args);
+    }
+
+}
