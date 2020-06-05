ba25363b6a85656c81ac9c65cc88e99fa9720e0c
Nguyen Phuc Thien An <npta97@gmail.com>
2020-06-05 14:54:52 +0700

    Config route for Beer Order Service

--- mssc-brewery-gateway/src/main/java/com/nguyenphucthienan/msscbrewerygateway/config/LocalHostRouteConfig.java
+++ mssc-brewery-gateway/src/main/java/com/nguyenphucthienan/msscbrewerygateway/config/LocalHostRouteConfig.java
@@ -14,6 +14,9 @@ public class LocalHostRouteConfig {
                 .route(route -> route.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
                         .id("beer-service"))
+                .route(route -> route.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
                 .build();
     }
 }
